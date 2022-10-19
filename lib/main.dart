80db1031111e7442a80cb9f876667f237e97444f
Johannes Jahn <johannes.jahn@outlook.com>
2022-10-19 14:40:20 +0200

    something

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:nacho_chat/pages/login.dart';
 import 'package:nacho_chat/service/app.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'pages/home.dart';
 
@@ -16,7 +16,6 @@ void main() async {
 class NachoChat extends StatelessWidget {
   const NachoChat({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     final initialRoute = AppService.instance.hive.get("access_token") == null

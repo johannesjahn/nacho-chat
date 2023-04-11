055fd138e431d888e0d7947446e0be3b51d17b00
Johannes Jahn <johannes.jahn@outlook.com>
2023-04-12 01:30:05 +0200

    Add chat to arb

--- lib/pages/chat.dart
+++ lib/pages/chat.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:nacho_chat/components/chat_view.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context)!;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Chat"),
+        title: Text(l10n.chat),
       ),
       body: const ChatView(),
     );

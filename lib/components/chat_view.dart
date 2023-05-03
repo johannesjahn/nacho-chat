7a7bbf69dcb506ed4a96febf1d1c30baf2f1a0b9
Johannes Jahn <johannes.jahn@outlook.com>
2023-05-03 20:09:15 +0200

    Code cleanup

--- lib/components/chat_view.dart
+++ lib/components/chat_view.dart
@@ -1,5 +1,3 @@
-import 'dart:io' show Platform;
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';

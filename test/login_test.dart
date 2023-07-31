ebee05121287627ccf178854ff43edd8844bb3c0
Johannes Jahn <johannes.jahn@outlook.com>
2023-07-31 10:48:13 +0200

    udpate useless test

--- test/login_test.dart
+++ test/login_test.dart
@@ -24,6 +24,6 @@ Widget makeTestableWidget({required Widget child}) {
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    expect(123, equals(123));
+    expect(1, equals(1));
   });
 }

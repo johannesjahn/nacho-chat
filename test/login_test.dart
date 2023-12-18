91a1052bb4aa47f45edfa55d8042d92c93261b02
Johannes Jahn <johannes.jahn@outlook.com>
2023-12-18 15:31:45 +0100

    Bump

--- test/login_test.dart
+++ test/login_test.dart
@@ -24,6 +24,6 @@ Widget makeTestableWidget({required Widget child}) {
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    expect(6, equals(7 - 1));
+    expect(5, equals(7 - 2));
   });
 }

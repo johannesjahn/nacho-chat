08b0172c32790c45ff2331c0eca828eb3ee5986c
Johannes Jahn <johannes.jahn@outlook.com>
2025-10-26 15:24:58 +0100

    fix avatar url

--- lib/service/constants.dart
+++ lib/service/constants.dart
@@ -1,16 +1,17 @@
 class Urls {
   Urls._();
 
-  static const url = "https://chat.johannes-jahn.com/";
+  static var url = "https://chat.johannes-jahn.com/";
   // static const url = "http://localhost:3000/";
 
   static String getUrl() {
     if (Uri.base.toString().startsWith("http") &&
         !Uri.base.toString().contains("localhost")) {
-      return Uri.base.toString();
+      url = Uri.base.toString();
+      return url;
     }
     return "https://chat.johannes-jahn.com/";
   }
 
-  static const avatar = "${url}app/user/avatar/";
+  static final avatar = "${url}app/user/avatar/";
 }
